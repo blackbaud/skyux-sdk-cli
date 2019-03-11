ed885ec0b2a2327cbe48d223e5f67dbc56885a85
Bobby Earl <bobby.earl@blackbaud.com>
2019-03-11 14:53:59 -0400

    Fixed logging of fails. (#11)
    * Fixed logging of fails.
    
    * Adjusted comment.
    
    * Another typo

--- bin/sky-pages.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-console.error('The "sky-pages" command has been deprecated.  Please use "skyux".');
--- test/bin-sky-pages.spec.js
+++
@@ -1,8 +0,0 @@
-describe('sky-pages bin', () => {
-  it('should call console.error', () => {
-    spyOn(console, 'error');
-    require('../bin/sky-pages');
-    /* eslint-disable no-console */
-    expect(console.error).toHaveBeenCalled();
-  });
-});

99f3026d8bc0719d494e7bc708072de0a29ca646
Jason Bodnar <jason.bodnar@blackbaud.com>
2020-04-30 14:11:12 -0500

    Add --dryrun flag to upgrade that lists upgradeable dependencies but doesn't do the upgrades

--- test/lib-upgrade.spec.js
+++ test/lib-upgrade.spec.js
@@ -14,7 +14,8 @@ describe('Upgrade', () => {
     };
 
     loggerMock = {
-      info: jasmine.createSpy('info')
+      info: jasmine.createSpy('info'),
+      error: jasmine.createSpy('error')
     };
 
     latestVersionMock = jasmine.createSpy('latestVersion').and.callFake((packageName) => {
@@ -42,6 +43,8 @@ describe('Upgrade', () => {
 
     mock.reRequire('../lib/app-dependencies');
     upgrade = mock.reRequire('../lib/upgrade');
+
+    spyOn(process, 'exit');
   });
 
   afterEach(() => {
@@ -161,5 +164,89 @@ describe('Upgrade', () => {
         }
       }
     );
+  });
+
+  it('should not upgrade an application but log upgradable dependencies when run with dryrun', async () => {
+    jsonUtilsMock.readJson.and.returnValue({
+      dependencies: {
+        '@foo/bar': '12.2.3'
+      }
+    });
+
+    await upgrade({ 'dryrun': true });
+
+    expect(latestVersionMock).toHaveBeenCalledWith(
+      '@foo/bar',
+      {
+        version: '12'
+      }
+    );
+    expect(jsonUtilsMock.writeJson).not.toHaveBeenCalled();
+    expect(cleanupMock.deleteDependencies).not.toHaveBeenCalled();
+    expect(loggerMock.error).toHaveBeenCalledWith(
+      jasmine.stringMatching(/This project has out of date depenedencies. Here are the out of date dependencies with their latest versions:/)
+    );
+    expect(loggerMock.error).toHaveBeenCalledWith(
+      jasmine.stringMatching(/@foo\/bar can be upgraded to version 12\.2\.5/)
+    );
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  it('should not upgrade an application but log upgradable devDependencies when run with dryrun', async () => {
+    jsonUtilsMock.readJson.and.returnValue({
+      devDependencies: {
+        '@foo/baz': '3.5.6',
+        'from-branch': 'foo/bar#branch'
+      }
+    });
+
+    await upgrade({ 'dryrun': true });
+
+    expect(latestVersionMock).toHaveBeenCalledWith(
+      '@foo/baz',
+      {
+        version: '3'
+      }
+    );
+    expect(jsonUtilsMock.writeJson).not.toHaveBeenCalled();
+    expect(cleanupMock.deleteDependencies).not.toHaveBeenCalled();
+    expect(loggerMock.error).toHaveBeenCalledWith(
+      jasmine.stringMatching(/This project has out of date depenedencies. Here are the out of date dependencies with their latest versions:/)
+    );
+    expect(loggerMock.error).toHaveBeenCalledWith(
+      jasmine.stringMatching(/@foo\/baz can be upgraded to version 4\.5\.6/)
+    );
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  it('should not upgrade an application and not log anything when run with dryrun and there are no out of date dependencies', async () => {
+    jsonUtilsMock.readJson.and.returnValue({
+      dependencies: {
+        '@foo/bar': '12.2.5'
+      },
+      devDependencies: {
+        '@foo/baz': '4.5.6',
+        'from-branch': 'foo/bar#branch'
+      }
+    });
+
+    await upgrade({ 'dryrun': true });
+
+    expect(latestVersionMock).toHaveBeenCalledWith(
+      '@foo/bar',
+      {
+        version: '12'
+      }
+    );
+    expect(latestVersionMock).toHaveBeenCalledWith(
+      '@foo/baz',
+      {
+        version: '4'
+      }
+    );
+    expect(jsonUtilsMock.writeJson).not.toHaveBeenCalled();
+    expect(cleanupMock.deleteDependencies).not.toHaveBeenCalled();
+    expect(loggerMock.error).not.toHaveBeenCalled();
+    expect(process.exit).not.toHaveBeenCalledWith(1);
   });
 });

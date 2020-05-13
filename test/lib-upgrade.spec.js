07e0b5f17988d7339a5e475c4dadcd8d4eae03ce
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-05-13 08:48:58 -0400

    Improve `skyux upgrade`

--- test/lib-upgrade.spec.js
+++ test/lib-upgrade.spec.js
@@ -1,46 +1,47 @@
 const mock = require('mock-require');
 
 describe('Upgrade', () => {
+  let appDependenciesMock;
   let jsonUtilsMock;
   let loggerMock;
-  let latestVersionMock;
-  let cleanupMock;
   let upgrade;
+  let npmInstallArgs;
+  let npmInstallMock;
+  let npmInstallCalled;
+  let processExitSpy;
 
   beforeEach(() => {
+    appDependenciesMock = {
+      async addSkyPeerDependencies() {},
+      async upgradeDependencies() {}
+    };
+
     jsonUtilsMock = {
       readJson: jasmine.createSpy('readJson'),
       writeJson: jasmine.createSpy('writeJson')
     };
 
+    npmInstallArgs = {};
+    npmInstallCalled = false;
+    npmInstallMock = function (args) {
+      npmInstallArgs = args;
+      npmInstallCalled = true;
+    };
+
     loggerMock = {
+      logLevel: undefined,
+      error() {},
       info: jasmine.createSpy('info')
     };
 
-    latestVersionMock = jasmine.createSpy('latestVersion').and.callFake((packageName) => {
-      switch (packageName) {
-        case '@foo/bar':
-          return '12.2.5';
-        case '@foo/baz':
-          return '4.5.6';
-        case 'typescript':
-          return '2.2';
-        case 'zone.js':
-          return '0.9.0';
-      }
-    });
-
-    cleanupMock = {
-      deleteDependencies: jasmine.createSpy('deleteDependencies')
-    };
+    processExitSpy = spyOn(process, 'exit');
 
     mock('@blackbaud/skyux-logger', loggerMock);
-    mock('latest-version', latestVersionMock);
 
     mock('../lib/utils/json-utils', jsonUtilsMock);
-    mock('../lib/cleanup', cleanupMock);
+    mock('../lib/app-dependencies', appDependenciesMock);
+    mock('../lib/utils/npm-install', npmInstallMock);
 
-    mock.reRequire('../lib/app-dependencies');
     upgrade = mock.reRequire('../lib/upgrade');
   });
 
@@ -48,118 +49,98 @@ describe('Upgrade', () => {
     mock.stopAll();
   });
 
-  it('should upgrade an application', async () => {
+  it('should upgrade an application', async (done) => {
+    const upgradeDependenciesSpy = spyOn(appDependenciesMock, 'upgradeDependencies').and.callThrough();
+    const skyDependenciesSpy = spyOn(appDependenciesMock, 'addSkyPeerDependencies').and.callThrough();
+
+    const dependencies = {
+      '@foo/bar': '12.2.3'
+    };
+
+    const devDependencies = {
+      '@foo/baz': '4.5.6',
+      'from-branch': 'foo/bar#branch'
+    };
+
     jsonUtilsMock.readJson.and.returnValue({
-      dependencies: {
-        '@foo/bar': '12.2.3'
-      },
-      devDependencies: {
-        '@foo/baz': '4.5.6',
-        'from-branch': 'foo/bar#branch'
-      }
+      dependencies,
+      devDependencies
     });
 
-    await upgrade();
+    await upgrade({});
 
-    expect(latestVersionMock).toHaveBeenCalledWith(
-      '@foo/bar',
-      {
-        version: '12'
-      }
-    );
+    expect(upgradeDependenciesSpy).toHaveBeenCalledWith(dependencies);
+    expect(upgradeDependenciesSpy).toHaveBeenCalledWith(devDependencies);
+    expect(skyDependenciesSpy).toHaveBeenCalledWith(dependencies);
+    expect(skyDependenciesSpy).toHaveBeenCalledWith(devDependencies);
+    expect(jsonUtilsMock.writeJson).toHaveBeenCalledWith('package.json', {
+      dependencies,
+      devDependencies
+    });
+    expect(npmInstallCalled).toEqual(true);
 
-    expect(latestVersionMock).toHaveBeenCalledWith(
-      '@foo/baz',
-      {
-        version: '4'
-      }
-    );
-
-    expect(jsonUtilsMock.writeJson).toHaveBeenCalledWith(
-      'package.json',
-      {
-        dependencies: {
-          '@foo/bar': '12.2.5'
-        },
-        devDependencies: {
-          '@foo/baz': '4.5.6',
-          'from-branch': 'foo/bar#branch'
-        }
-      }
-    );
-
-    expect(cleanupMock.deleteDependencies).toHaveBeenCalled();
+    done();
   });
 
-  it('should not upgrade TypeScript', async () => {
+  it('should not run npm install if --no-install provided', async (done) => {
+    const dependencies = {
+      '@foo/bar': '12.2.3'
+    };
+
     jsonUtilsMock.readJson.and.returnValue({
-      devDependencies: {
-        'typescript': '2.1'
-      }
+      dependencies
     });
 
-    await upgrade();
+    await upgrade({
+      install: false
+    });
 
-    expect(jsonUtilsMock.writeJson).toHaveBeenCalledWith(
-      'package.json',
-      {
-        devDependencies: {
-          'typescript': '2.1'
-        }
-      }
-    );
+    expect(jsonUtilsMock.writeJson).toHaveBeenCalledWith('package.json', { dependencies });
+    expect(npmInstallCalled).toEqual(false);
 
-    expect(loggerMock.info).toHaveBeenCalledWith(
-      jasmine.stringMatching(/This project includes a reference to/)
-    );
+    done();
   });
 
-  it('should not upgrade zone.js', async () => {
+  it('should pass stdio: inherit to spawn when logLevel is verbose', async (done) => {
+    loggerMock.logLevel = 'verbose';
+
+    const dependencies = {
+      '@foo/bar': '12.2.3'
+    };
+
     jsonUtilsMock.readJson.and.returnValue({
-      dependencies: {
-        'zone.js': '0.8.29'
-      }
+      dependencies
     });
 
-    await upgrade();
+    await upgrade({
+      install: true
+    });
 
-    expect(jsonUtilsMock.writeJson).toHaveBeenCalledWith(
-      'package.json',
-      {
-        dependencies: {
-          'zone.js': '0.8.29'
-        }
-      }
-    );
+    expect(npmInstallCalled).toEqual(true);
+    expect(npmInstallArgs).toEqual({
+      stdio: 'inherit'
+    });
 
-    expect(loggerMock.info).toHaveBeenCalledWith(
-      jasmine.stringMatching(/This project includes a reference to/)
-    );
+    done();
   });
 
-  it('should handle missing dependency section', async () => {
+  it('should handle errors', async (done) => {
+    spyOn(appDependenciesMock, 'upgradeDependencies').and.throwError(
+      'Something bad happened.'
+    );
+
     jsonUtilsMock.readJson.and.returnValue({
-      devDependencies: {
-        '@foo/bar': '12.2.3'
-      }
+      dependencies: {},
+      devDependencies: {}
     });
 
-    await upgrade();
+    const loggerSpy = spyOn(loggerMock, 'error').and.callThrough();
 
-    expect(latestVersionMock).toHaveBeenCalledWith(
-      '@foo/bar',
-      {
-        version: '12'
-      }
-    );
+    await upgrade({});
 
-    expect(jsonUtilsMock.writeJson).toHaveBeenCalledWith(
-      'package.json',
-      {
-        devDependencies: {
-          '@foo/bar': '12.2.5'
-        }
-      }
-    );
+    expect(loggerSpy).toHaveBeenCalledWith('Error: Something bad happened.');
+    expect(processExitSpy).toHaveBeenCalledWith(1);
+
+    done();
   });
 });

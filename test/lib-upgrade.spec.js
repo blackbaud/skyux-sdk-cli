ee0b50e691be4ec057d005c6a5e66116710538b7
Steve Brush <steve.brush@blackbaud.com>
2020-06-05 16:10:35 -0400

    [rc] `skyux upgrade` > Backwards compat (#85)

--- test/lib-upgrade.spec.js
+++ test/lib-upgrade.spec.js
@@ -2,6 +2,7 @@ const mock = require('mock-require');
 
 describe('Upgrade', () => {
   let appDependenciesMock;
+  let appDependenciesV3Mock;
   let jsonUtilsMock;
   let loggerMock;
   let upgrade;
@@ -9,9 +10,15 @@ describe('Upgrade', () => {
   let npmInstallMock;
   let npmInstallCalled;
   let processExitSpy;
+  let latestVersionMock;
 
   beforeEach(() => {
     appDependenciesMock = {
+      async addSkyPeerDependencies() {},
+      async upgradeDependencies() {}
+    };
+
+    appDependenciesV3Mock = {
       async addSkyPeerDependencies() {},
       async upgradeDependencies() {}
     };
@@ -20,6 +27,10 @@ describe('Upgrade', () => {
       readJson: jasmine.createSpy('readJson'),
       writeJson: jasmine.createSpy('writeJson')
     };
+
+    latestVersionMock = jasmine.createSpy('latestVersion').and.callFake((packageName, args) => {
+      return Promise.resolve(args.version);
+    });
 
     npmInstallArgs = {};
     npmInstallCalled = false;
@@ -37,9 +48,11 @@ describe('Upgrade', () => {
     processExitSpy = spyOn(process, 'exit');
 
     mock('@blackbaud/skyux-logger', loggerMock);
+    mock('latest-version', latestVersionMock);
 
     mock('../lib/utils/json-utils', jsonUtilsMock);
     mock('../lib/app-dependencies', appDependenciesMock);
+    mock('../lib/v3-compat/app-dependencies', appDependenciesV3Mock);
     mock('../lib/utils/npm-install', npmInstallMock);
 
     upgrade = mock.reRequire('../lib/upgrade');
@@ -59,6 +72,7 @@ describe('Upgrade', () => {
 
     const devDependencies = {
       '@foo/baz': '4.5.6',
+      '@skyux-sdk/builder': '4.0.0',
       'from-branch': 'foo/bar#branch'
     };
 
@@ -87,15 +101,23 @@ describe('Upgrade', () => {
       '@foo/bar': '12.2.3'
     };
 
+    const devDependencies = {
+      '@skyux-sdk/builder': '4.0.0'
+    };
+
     jsonUtilsMock.readJson.and.returnValue({
-      dependencies
+      dependencies,
+      devDependencies
     });
 
     await upgrade({
       install: false
     });
 
-    expect(jsonUtilsMock.writeJson).toHaveBeenCalledWith('package.json', { dependencies });
+    expect(jsonUtilsMock.writeJson).toHaveBeenCalledWith('package.json', {
+      dependencies,
+      devDependencies
+    });
     expect(npmInstallCalled).toEqual(false);
 
     done();
@@ -109,7 +131,10 @@ describe('Upgrade', () => {
     };
 
     jsonUtilsMock.readJson.and.returnValue({
-      dependencies
+      dependencies,
+      devDependencies: {
+        '@skyux-sdk/builder': '4.0.0'
+      }
     });
 
     await upgrade({
@@ -131,7 +156,9 @@ describe('Upgrade', () => {
 
     jsonUtilsMock.readJson.and.returnValue({
       dependencies: {},
-      devDependencies: {}
+      devDependencies: {
+        '@skyux-sdk/builder': '4.0.0'
+      }
     });
 
     const loggerSpy = spyOn(loggerMock, 'error').and.callThrough();
@@ -140,6 +167,23 @@ describe('Upgrade', () => {
 
     expect(loggerSpy).toHaveBeenCalledWith('Error: Something bad happened.');
     expect(processExitSpy).toHaveBeenCalledWith(1);
+
+    done();
+  });
+
+  it('should be backwards compatible with Builder 3 projects', async (done) => {
+    const upgradeDependenciesV3Spy = spyOn(appDependenciesV3Mock, 'upgradeDependencies').and.callThrough();
+
+    jsonUtilsMock.readJson.and.returnValue({
+      dependencies: {},
+      devDependencies: {
+        '@skyux-sdk/builder': '3.0.0' // <-- IMPORTANT
+      }
+    });
+
+    await upgrade({});
+
+    expect(upgradeDependenciesV3Spy).toHaveBeenCalled();
 
     done();
   });

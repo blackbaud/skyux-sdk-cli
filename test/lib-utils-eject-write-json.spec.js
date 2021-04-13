3fa001202937c85a939aeb93f6b4071a2ecfa045
Paul Crowder <paul.crowder@blackbaud.com>
2021-04-13 14:24:44 -0400

    Ensure resources_en_US.json exists (#116)
    This fixes a bug where creating the "Not Found" component failed when resources_en_US.json did not exist.

---
+++ test/lib-utils-eject-write-json.spec.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const mock = require('mock-require');
+
+describe('writeJson', () => {
+  let writeJson;
+  let mockFsExtra;
+
+  beforeEach(() => {
+    mockFsExtra = jasmine.createSpyObj(
+      'fs-extra',
+      ['ensureDirSync', 'writeJsonSync']
+    )
+
+    mock('fs-extra', mockFsExtra);
+
+    writeJson = mock.reRequire('../lib/utils/eject/write-json');
+  });
+
+  afterEach(() => {
+    mock.stopAll();
+  });
+
+  it('should write the specified JSON with the epxected format options', () => {
+    const filePath = path.join('foo', 'bar.json');
+    const contents = {
+      foo: 'bar'
+    };
+
+    writeJson(filePath, contents);
+
+    expect(mockFsExtra.writeJsonSync).toHaveBeenCalledWith(
+      filePath,
+      contents,
+      {
+        spaces: 2
+      }
+    );
+  });
+
+  it('should create the directory for the JSON file if it does not exist', () => {
+    writeJson(
+      path.join('foo', 'bar.json'),
+      {
+        foo: 'bar'
+      }
+    );
+
+    expect(mockFsExtra.ensureDirSync).toHaveBeenCalledWith('foo');
+  });
+});

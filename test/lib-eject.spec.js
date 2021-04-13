2f3c7a155f81487a18344c1fa303a2c6e16a3a5f
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-04-12 22:33:49 -0400

    Fix child paths

--- test/lib-eject.spec.js
+++ test/lib-eject.spec.js
@@ -578,9 +578,9 @@ const routes: Routes = [
   { path: '', component: RootRouteIndexComponent },
   { path: 'about/careers', component: AboutCareersRouteIndexComponent },
   { path: 'about', component: AboutRouteIndexComponent, children: [
-    { path: 'about/contact', component: AboutContactRouteIndexComponent, children: [
-      { path: 'about/contact/form', component: AboutContactFormRouteIndexComponent },
-      { path: 'about/contact/contributors', component: AboutContactContributorsRouteIndexComponent }
+    { path: 'contact', component: AboutContactRouteIndexComponent, children: [
+      { path: 'form', component: AboutContactFormRouteIndexComponent },
+      { path: 'contributors', component: AboutContactContributorsRouteIndexComponent }
     ] }
   ] },
   { path: 'users/:userId', component: UsersUserIdRouteIndexComponent },
